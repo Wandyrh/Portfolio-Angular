65287a71792d3c8596edcab3cbe58722b4ef6311
Wandy Rodríguez <wandyrh@hotmail.com>
2025-06-14 18:00:04 -0400

    implement bootstrap (#3)

--- src/app/layouts/main-layout/main-layout.ts
+++ src/app/layouts/main-layout/main-layout.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
 import { Sidebar } from './sidebar/sidebar';
 import { HeaderComponent } from './header/header';
 
@@ -13,10 +9,6 @@ import { HeaderComponent } from './header/header';
   imports: [
     RouterOutlet,
     Sidebar,
-    MatSidenavModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
     HeaderComponent
   ],
   templateUrl: './main-layout.html',
--- src/app/layouts/main-layout/sidebar/sidebar.ts
+++ src/app/layouts/main-layout/sidebar/sidebar.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { MatNavList, MatListItem } from '@angular/material/list';
-import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [MatNavList, MatListItem, MatIconModule, RouterModule],
+  imports: [RouterModule],
   templateUrl: './sidebar.html',
   styleUrls: ['./sidebar.scss']
 })
